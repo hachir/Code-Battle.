e56d15013d63f83260648086013115e7c489d979
hachir <ha1991ha@yahoo.com>
2019-07-09 21:17:23 -0600

    update-fiels

--- server.js
+++ server.js
@@ -41,3 +41,18 @@ app.use(passport.session()); // calls the deserializeUser
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+// Routes
+app.use("/user", userRoute);
+app.use("/api/question", questionRoute);
+
+
+// Send every request to the React app
+// Define any API routes before this runs
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+
+app.listen(PORT, function () {
+  console.log(`🌎 ==> Server now on port ${PORT}!`);
+});
