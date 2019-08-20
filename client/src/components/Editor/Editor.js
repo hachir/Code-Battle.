3a5f938063e0729b9ce306acf826f0a5cc79f65b
hachir <ha1991ha@yahoo.com>
2019-08-20 15:12:07 -0600

    Editor.js

---
+++ client/src/components/Editor/Editor.js
@@ -0,0 +1,39 @@
+import React from "react";
+import AceEditor from "react-ace";
+import "./Editor.css";
+
+import "brace/mode/javascript";
+import "brace/theme/github";
+import "brace/theme/monokai";
+
+
+const Editor = (props) => (
+    <div className="editorWrapper" onKeyDown={props.keySubmit}>
+      <AceEditor
+          mode="javascript"
+          theme="monokai"
+          name="editor"
+          onLoad={this.onLoad}
+          onChange={props.change}
+          fontSize={18}
+          showPrintMargin={true}
+          showGutter={true}
+          highlightActiveLine={true}
+          width="100%"
+          height="100%"
+          value={props.code}
+          setOptions={{
+            enableBasicAutocompletion: true,
+            enableLiveAutocompletion: false,
+            showPrintMargin: false,
+            enableSnippets: false,
+            showLineNumbers: true,
+            tabSize: 2,
+          }}/>
+      <button className="submitCode" onClick={props.submit}>Submit (ctrl +
+        enter)
+      </button>
+    </div>
+);
+
+export default Editor;
