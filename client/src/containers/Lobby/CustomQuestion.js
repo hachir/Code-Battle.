cff159f83c5719d11e42006378165ab9ee4906ed
hachir <ha1991ha@yahoo.com>
2019-08-31 14:34:04 -0600

    CustomQuestion.js

---
+++ client/src/containers/Lobby/CustomQuestion.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../Lobby.css';
+
+const CustomQuestion = props => {
+	return (
+		<div className={'custom-question'}>
+			<button
+				className="gray"
+				name="custom-question"
+				onClick={props.onClick}
+				type="button"
+				disabled={props.disabled}
+			>
+				Create your own Question
+			</button>
+		</div>
+	);
+};
+
+export default CustomQuestion;
