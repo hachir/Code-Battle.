47e97f3fef75258f11132e28c1d2375d75113794
hachir <ha1991ha@yahoo.com>
2019-12-11 13:06:57 -0700

    fix Footer.js

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -7,20 +7,14 @@ const footer = () => (
     <footer className="footer">
       <p>Copyright &copy; 2019 Code Battle
         <a
-            href="https://github.com/maxrjohnson23/Code-Battle"
+            href="https://github.com/hachir/Code-Battle."
             target="_blank" rel="noopener noreferrer">
           <FaGithubSquare size={20} className="github-icon"/>
         </a>
       </p>
       <p>Created by:
-        <a href="https://github.com/maxrjohnson23" target="_blank"
-           rel="noopener noreferrer">MJ</a>
-        <a href="https://github.com/zachtjohnson01" target="_blank"
-           rel="noopener noreferrer">ZJ</a>
-        <a href="https://github.com/pat-knight" target="_blank"
-           rel="noopener noreferrer">PK</a>
-        <a href="https://github.com/Gaccettu" target="_blank"
-           rel="noopener noreferrer">GA</a>
+        <a href="https://github.com/hachir/Code-Battle." target="_blank"
+           rel="noopener noreferrer">HA</a>
       </p>
     </footer>
 );
